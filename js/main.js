172e3b13d19a2916d4c4e4c2a85be1e6ed83df25
Snazzy <stankovicnino@gmail.com>
2018-08-17 23:39:36 +0200

    navigation fixed, added overlay

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const navItems = document.querySelectorAll(".nav-item");
+const overlay = document.querySelector(".overlay");
 
 let showMenu = false;
 
@@ -16,6 +17,7 @@ function toggleMenu() {
         menuBtn.classList.add("close");
         menu.classList.add("show");
         menuNav.classList.add("show");
+        overlay.classList.add("show");
         navItems.forEach(item => item.classList.add("show"));
 
         showMenu = true;
@@ -23,10 +25,21 @@ function toggleMenu() {
         menuBtn.classList.remove("close");
         menu.classList.remove("show");
         menuNav.classList.remove("show");
+        overlay.classList.remove("show");
         navItems.forEach(item => item.classList.remove("show"));
 
         showMenu = false;
     }
+}
+
+overlay.addEventListener("click", clearMenu);
+
+function clearMenu() {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    overlay.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
 }
 
 // ------------------
@@ -39,3 +52,47 @@ var typed = new Typed(".type", {
     backSpeed: 50,
     loop: true
 });
+
+
+// ------------------
+// SMOOTH SCROLL
+// ------------------
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+
